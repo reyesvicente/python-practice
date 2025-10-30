0da497319057a7487539f3493af701ae2e4ed239
Vicente Reyes <icenreyes@icloud.com>
2025-10-30 11:56:53 +0800

    finished sum_of_digits

--- sum_of_digits.py
+++ sum_of_digits.py
@@ -6,4 +6,19 @@
 # Example: sum_of_digits(9999) should return 36
 
 def sum_of_digits(n):
-    pass
+    """
+        PSEUDO CODE:
+        1. Initialize a variable to store the sum of digits (e.g., sum = 0)
+        2. Use a loop to iterate through each digit of the number
+        3. Convert the number to a string to access each digit
+        4. Convert each digit back to an integer and add it to the sum
+        5. Return the sum of the digits
+    """
+    sum = 0
+    for digit in str(n):
+        sum += int(digit)
+    return sum
+
+
+print(sum_of_digits(144))
+print(sum_of_digits(9999))
