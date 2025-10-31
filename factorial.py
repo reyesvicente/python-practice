f80c6f6d7ebc585298bdec6b203ae8bbfc98d5cc
Vicente Reyes <icenreyes@yahoo.com>
2025-10-31 19:53:20 +0800

    factorial done

--- factorial.py
+++ factorial.py
@@ -7,4 +7,24 @@
 # Example: factorial(0) should return 1
 
 def factorial(n):
-    pass
+    if n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+    """
+        PSEUDO CODE:
+            IF n is 0 OR n is 1:
+            RETURN 1
+        ELSE:
+            result = 1
+            FOR i FROM 2 TO n:
+                result = result * i
+            RETURN result
+    """
+
+print(factorial(5))
+print(factorial(0))
+print(factorial(7))
