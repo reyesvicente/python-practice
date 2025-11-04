e3da9384648f0d37f6f5371ca8ebfcf2f9e08cec
Vicente Reyes <icenreyes@yahoo.com>
2025-11-04 15:15:09 +0800

    is_prime done

--- part_2/is_prime.py
+++ part_2/is_prime.py
@@ -8,4 +8,31 @@
 # Example: is_prime(2) should return True
 
 def is_prime(n):
-    pass
+    """
+        IF n <= 1:
+        RETURN False
+        IF n == 2:
+            RETURN True
+        IF n is even:
+            RETURN False
+
+        # Check for divisors up to square root of n
+        FOR i FROM 3 TO SQUARE_ROOT(n) STEP 2:
+            IF n % i == 0:
+                RETURN False
+    """
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(7))
+print(is_prime(10))
+print(is_prime(2))
