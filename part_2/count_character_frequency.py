83f7b6516139b46bd73f0b7043ee0adae434b5c5
Vicente Reyes <icenreyes@yahoo.com>
2025-11-05 17:25:15 +0800

    frequency counter done

--- part_2/count_character_frequency.py
+++ part_2/count_character_frequency.py
@@ -6,4 +6,32 @@
 # Example: char_frequency("hello world") should return {'h': 1, 'e': 1, 'l': 3, 'o': 2, 'w': 1, 'r': 1, 'd': 1}
 
 def char_frequency(s):
-    pass
+    """
+        FUNCTION char_frequency(INPUT string s)
+            CREATE empty dictionary freq_dict
+
+            CONVERT s to lowercase
+            REMOVE all spaces from s
+
+            FOR each character c in s DO
+                IF c is already a key in freq_dict THEN
+                    INCREMENT freq_dict[c] by 1
+                ELSE
+                    SET freq_dict[c] = 1
+                END IF
+            END FOR
+
+            RETURN freq_dict
+        END FUNCTION
+    """
+    freq_dict = {}
+    s = s.lower().replace(" ", "")
+
+    for char in s:
+        if char in freq_dict:
+            freq_dict[char] += 1
+        else:
+            freq_dict[char] = 1
+    return freq_dict
+
+print(char_frequency("hello world") )
