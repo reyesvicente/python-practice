57fc28d14da421d5e0e631b67a0d070dd4cf7219
Vicente Reyes <icenreyes@yahoo.com>
2025-11-09 12:48:43 +0800

    finished find_pairs

--- part_2/find_pairs.py
+++ part_2/find_pairs.py
@@ -7,4 +7,25 @@
 # Example: find_pairs([2, 3, 4, 5], 9) should return [(4, 5)]
 
 def find_pairs(lst, target):
-    pass
+    """
+        PSUEDO CODE:
+        1. Initialize an empty set to store unique numbers
+        2. Initialize an empty list to store pairs
+        3. Iterate through the list
+        4. For each number, calculate the complement (target - number)
+        5. If the complement is in the set, add the pair to the list
+        6. Add the number to the set
+        7. Return the list of pairs
+    """
+    seen = set()
+    pairs_found = set()
+    for num in lst:
+        complement = target - num
+        if complement in seen:
+            pairs_found.add((min(num, complement), max(num, complement)))
+        seen.add(num)
+    return list(pairs_found)
+
+print(find_pairs([1, 5, 7, -1, 5], 6))
+print(find_pairs([2, 3, 4, 5], 9))
+
