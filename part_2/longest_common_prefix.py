894ebb17d97485cb4eaffdf637d985a9f4ad23b2
Vicente Reyes <icenreyes@yahoo.com>
2025-11-09 19:32:42 +0800

    solved longest common prefix

--- part_2/longest_common_prefix.py
+++ part_2/longest_common_prefix.py
@@ -7,4 +7,33 @@
 # Example: longest_common_prefix(['interspecies', 'interstellar']) should return 'inters'
 
 def longest_common_prefix(strings):
-    pass
+    """
+        PSEUDO CODE:
+            function longest_common_prefix(strings):
+            if strings is empty:
+                return empty string
+
+            set prefix to the first string in strings
+
+            for each string s in strings starting from the second string:
+                while s does not start with prefix:
+                    remove the last character from prefix
+                    if prefix is empty:
+                        return empty string
+
+            return prefix
+    """
+    if strings is None or strings == 0:
+        return ""
+
+    prefix = strings[0]
+    for s in strings[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if prefix == "":
+                return ""
+    return prefix
+
+print(longest_common_prefix(['flower', 'flow', 'flight']))
+print(longest_common_prefix(['dog', 'racecar', 'car']))
+print(longest_common_prefix(['interspecies', 'interstellar']))
