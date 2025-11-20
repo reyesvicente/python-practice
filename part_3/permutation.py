8d9f8b3828e3b37143439dc079bcc5cda8d90768
Vicente Reyes <icenreyes@yahoo.com>
2025-11-20 13:30:43 +0800

    permutations done

--- part_3/permutation.py
+++ part_3/permutation.py
@@ -7,4 +7,61 @@
 # Hint: You can use recursion or import from itertools
 
 def permutations(lst):
-    pass
+    """
+        PSEUDO CODE:
+            FUNCTION permutations(lst):
+                // Base case: empty list or single element
+                IF length of lst <= 1:
+                    RETURN [tuple(lst)]
+
+                // Initialize result list
+                result = empty list
+
+                // For each element in the list
+                FOR i FROM 0 TO length of lst - 1:
+                    // Select current element as the "fixed" element
+                    current_element = lst[i]
+
+                    // Create remaining list (all elements except current)
+                    remaining_elements = lst[0:i] + lst[i+1:end]
+
+                    // Recursively get all permutations of remaining elements
+                    sub_permutations = permutations(remaining_elements)
+
+                    // For each sub-permutation, prepend the current element
+                    FOR each sub_perm IN sub_permutations:
+                        // Combine current element with sub-permutation
+                        new_permutation = (current_element) + sub_perm
+
+                        // Add to result
+                        ADD new_permutation TO result
+
+                RETURN result
+            END FUNCTION
+    """
+    if len(lst) <= 1:
+        return [tuple(lst)]
+
+        result = []
+
+        for i in range(len(lst)):
+            current_element = lst[i]
+            remaining_elements = lst[:i] + lst[i+1:]
+            sub_permutations = permutations(remaining_elements)
+
+            for sub_perm in sub_permutations:
+                new_permutation = (current_element,) + sub_perm
+                result.append(new_permutation)
+        return result
+    result = []
+    for i in range(len(lst)):
+        current_element = lst[i]
+        remaining_elements = lst[:i] + lst[i+1:]
+        sub_permutations = permutations(remaining_elements)
+
+        for sub_perm in sub_permutations:
+            new_permutation = (current_element,) + sub_perm
+            result.append(new_permutation)
+    return result
+
+print(permutations([1, 2, 3]))
